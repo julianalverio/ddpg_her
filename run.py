86d422f3fd758469f2e7141723980f70bbb670e6
julianalverio <julian.a.alverio@gmail.com>
2019-07-27 19:40:45 -0400

    iterating

--- run.py
+++ run.py
@@ -148,6 +148,7 @@ def main():
     PARAMS['log_dir'] = 'runs/env=%s_seed=%s' % (args.env, seed)
     shutil.rmtree(PARAMS['log_dir'], ignore_errors=True)
     PARAMS['save'] = args.save
+    PARAMS['env'] = args.env
     print('logging to:', PARAMS['log_dir'])
     writer = SummaryWriter(PARAMS['log_dir'])
 
