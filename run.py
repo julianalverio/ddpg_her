2c822f63d11dbce0fe05e1f52fbc24373f7e3379
julianalverio <julian.a.alverio@gmail.com>
2019-07-28 22:33:28 -0400

    iterating

--- run.py
+++ run.py
@@ -147,7 +147,7 @@ def main():
     test_env = gym.make('FetchPickAndPlace-v1', reward_type='sparse')
     test_env.reset()
     test_env.render(mode='human')
-    assert test_env.render(mode='rgb_array')
+    assert test_env.render(mode='rgb_array') is not None
     import pdb; pdb.set_trace()
 
     choose_gpu()
