c45040b03e6b9bbb6e38cb7eed7803b0571c5ac8
julianalverio <julian.a.alverio@gmail.com>
2019-08-05 12:58:37 -0400

    iterating

--- run.py
+++ run.py
@@ -144,11 +144,11 @@ def generate_videos(evaluator, args):
 
 def main():
     # test
-    test_env = gym.make('FetchPush-v1', reward_type='sparse')
-    test_env.reset()
-    test_env.step([0, 0, 0, 0])
-    test_env.render(mode='human')
-    assert test_env.render(mode='rgb_array') is not None
+    # test_env = gym.make('FetchPush-v1', reward_type='sparse')
+    # test_env.reset()
+    # test_env.step([0, 0, 0, 0])
+    # test_env.render(mode='human')
+    # assert test_env.render(mode='rgb_array') is not None
 
     import pdb; pdb.set_trace()
     test_env = gym.make('FetchPush-v1', reward_type='visual')
