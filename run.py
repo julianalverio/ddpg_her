e8396b0ad130b26775fc7981f72dec2780e52e16
julianalverio <julian.a.alverio@gmail.com>
2019-08-05 13:00:10 -0400

    iterating

--- run.py
+++ run.py
@@ -148,7 +148,7 @@ def main():
     test_env.reset()
     test_env.step([0, 0, 0, 0])
     # test_env.render(mode='human')
-    # assert test_env.render(mode='rgb_array') is not None
+    assert test_env.render(mode='rgb_array') is not None
 
     import pdb; pdb.set_trace()
     test_env = gym.make('FetchPush-v1', reward_type='visual')
