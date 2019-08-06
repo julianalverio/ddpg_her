adc8f49c082ccec4027b59249e0ceeb4182521e3
julianalverio <julian.a.alverio@gmail.com>
2019-08-06 09:37:04 -0400

    iterating

--- server.py
+++ server.py
@@ -19,8 +19,8 @@ class S(BaseHTTPRequestHandler):
     def do_POST(self):
         self._set_headers()
         print("Message received")
-        # data = self.rfile.read(int(self.headers['Content-Length']))
-        # print(data)
+        data = self.rfile.read(int(self.headers['Content-Length']))
+        print(data)
 
         # self.send_response(200, message='hello')
         # self.end_headers()
