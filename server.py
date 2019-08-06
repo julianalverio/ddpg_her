9eb4dd88c3eeed7a369e9221dd5c2b53506b6c55
julianalverio <julian.a.alverio@gmail.com>
2019-08-06 10:54:05 -0400

    iterating

--- server.py
+++ server.py
@@ -47,7 +47,7 @@ class S(BaseHTTPRequestHandler):
 def run(server_class=HTTPServer, handler_class=S, port=500):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
-    print('Starting httpd...')
+    print('Starting server...')
     httpd.serve_forever()
 
 if __name__ == "__main__":
