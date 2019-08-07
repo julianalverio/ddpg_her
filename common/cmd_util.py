51a7907d4c5d089805a5bc55a38a23b20928ba69
julianalverio <julian.a.alverio@gmail.com>
2019-08-07 12:19:51 -0400

    iterating

--- common/cmd_util.py
+++ common/cmd_util.py
@@ -58,7 +58,7 @@ def make_env(env_id, env_type, reward_type, mpi_rank=0, subrank=0, seed=None, re
     env = gym.make(env_id, reward_type)
     env.seed(seed + subrank)
     # import pdb; pdb.set_trace()
-    env = Monitor(env,
+    cenv = Monitor(env,
                   logger_dir and os.path.join(logger_dir, str(mpi_rank) + '.' + str(subrank)),
                   allow_early_resets=True)
     return env
