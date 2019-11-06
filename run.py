de211752023bac823c10c6d54aa255b0f3e23fcb
julianalverio <julian.a.alverio@gmail.com>
2019-11-06 04:19:31 -0500

    iterating

--- run.py
+++ run.py
@@ -144,7 +144,7 @@ def make_videos(model_dir, policy, env_name):
     frames = []
     for _ in range(50):
         import pdb; pdb.set_trace()
-        actions = policy.get_actions(obs, goal, 0, 0)
+        actions = policy.get_actions(obs, goal, 0, 0).cpu().detach()
         obs_dict_new, _, done, info = env.step(actions)
         obs = torch.tensor(obs_dict_new['observation']).cuda()
         obs = torch.unsqueeze(obs, 0)
